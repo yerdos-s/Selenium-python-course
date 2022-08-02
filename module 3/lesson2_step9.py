b2b8da70468a0c51fcf36e786d2d46327c93f622
Yerdos <erdos.sadvakasov@gmail.com>
2022-08-02 20:10:48 +0600

    lesson2_step9.py added

---
+++ module 3/lesson2_step9.py
@@ -0,0 +1,6 @@
+def test_substring(full_string, substring):
+    assert substring in full_string, \
+        f"expected '{substring}' to be substring of '{full_string}'"
+
+if __name__ == "__main__":
+    test_substring('sad','s')
