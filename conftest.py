400349fe151a60d57d65254b0b1b61b9343a7e54
Yerdos <erdos.sadvakasov@gmail.com>
2022-09-13 19:24:39 +0600

    conftest is edited (pytest_addoption and request added)

--- conftest.py
+++ conftest.py
@@ -1,12 +1,26 @@
 import pytest
 from selenium import webdriver
 
-@pytest.fixture
-def browser():
-    print("\nstart browser for test..")
-    options = webdriver.ChromeOptions()
-    options.add_experimental_option('excludeSwitches', ['enable-logging'])
-    driver = webdriver.Chrome(options=options)
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default="chrome",
+                     help="Choose browser: chrome or firefox")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    browser = None
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option('excludeSwitches', ['enable-logging'])
+        driver = webdriver.Chrome(options=options)
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        driver = webdriver.Firefox()
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield driver
     print("\nquit browser..")
     driver.quit()
